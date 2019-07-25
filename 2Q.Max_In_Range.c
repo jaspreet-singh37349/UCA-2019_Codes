fd94f7185b2bf30d794b5d3af4bfaec458293e59
jaspreet-singh37349 <42461489+jaspreet-singh37349@users.noreply.github.com>
2019-07-25 18:12:36 +0000

    2nd problem added

---
+++ 2Q.Max_In_Range.c
@@ -0,0 +1,68 @@
+/*
+Here two arrays of same size will be given and 1st element of 1st array will specify start of range and 1st element
+of 2nd array will specify end of range.
+Now we have to find out which element has occured for the most time.
+
+e.g.
+a = [2,1,3]
+b = [5,3,9]
+
+Now we have to find element which has occured for the most time in range 2-5,1-3,3-9
+
+output -> 3
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+    
+    int x,n,i,max=-99999,m=-99999;
+    scanf("%d",&n);
+
+    int L[n],R[n];
+    
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&L[i]);
+        scanf("%d",&R[i]);
+    }
+    
+    for(i=0;i<n;i++)
+    {
+        if(max<R[i])
+        {
+            max=R[i];
+        }
+    }
+    
+    int arr[max+1];
+    
+    for(i=0;i<=max;i++)
+    {
+        arr[i]=0;
+    }
+    
+    
+    
+    for(i=0;i<n;i++)
+    {
+        arr[L[i]]++;
+        arr[R[i]+1]--;
+    }
+    
+    
+    for(i=1;i<=max;i++)
+    {
+        arr[i]+=arr[i-1];
+        if(m<arr[i])
+            m=i;
+    }
+    
+    
+    printf("%d",m);
+    
+    return 0;
+}
