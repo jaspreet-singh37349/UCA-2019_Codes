a90dd401b62e9fb1009df5203158599fdfae48fe
jaspreet-singh37349 <42461489+jaspreet-singh37349@users.noreply.github.com>
2019-07-25 18:01:18 +0000

    added 1st problem

---
+++ 1Q. ReverseInGroups.c
@@ -0,0 +1,53 @@
+/*
+Reversing elements along with groups in array
+e.g.
+array = [8,7,6,5,4,3,2,1]
+We have to reverse in group of 3 then ->
+output -> [6,7,8] [3,4,5] [1,2] 
+
+We have to do this in complexity  = O(N)
+*/
+
+#include <stdio.h>
+
+int main()
+{   
+    int k,n,i,j,m;
+    scanf("%d",&n);
+    scanf("%d",&k);
+    
+    int arr[n];
+    
+    for(i=0;i<n;i++)
+    {
+        arr[i]=i+1;
+    }
+    
+    for(i=0;i<n;i+=k)
+    {
+        if(i+k<=n)
+        {
+            for(j=0;j<k/2;j++)
+            {
+                m=arr[i+j];
+                arr[i+j]=arr[i+k-1-j];
+                arr[i+k-1-j]=m;
+            }
+        }
+        else
+        {
+            for(j=0;j<(n%k)/2;j++)
+            {
+                m=arr[i+j];
+                arr[i+j]=arr[i+(n%k)-1-j];
+                arr[i+(n%k)-1-j]=m;
+            }
+        }
+    }
+
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
