575db2ba875285b5381b50409e73d6dda0e48082
jaspreet-singh37349 <42461489+jaspreet-singh37349@users.noreply.github.com>
2019-08-04 14:26:53 +0000

    updated

--- LinkedList_InsertAtBeg.c
+++ LinkedList_InsertAtBeg.c
@@ -1,10 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include <stdio.h>
 
@@ -14,7 +7,7 @@ struct node
     struct node *next;
 };
 
-void InsertAtBeg(int **h,int no)
+void InsertAtBeg(struct node **h,int no)
 {
     struct node* new_node = (struct node *) malloc(sizeof(struct node));
     
