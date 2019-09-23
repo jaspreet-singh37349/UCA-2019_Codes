3aed2cb24fe78cff2cb040f2cc0f626198b37c28
Jaspreet Singh <42461489+jaspreet-singh37349@users.noreply.github.com>
2019-09-23 15:43:27 +0000

    bst inorder

---
+++ bst/inorder.c
@@ -0,0 +1,74 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct bst{
+  int data;
+  struct bst* left;
+  struct bst* right;
+}bst;
+
+bst* createTree(bst* root,int data)
+{
+    if(root==NULL)
+    {
+        bst* new_node = (bst*)malloc(sizeof(bst));
+        
+        new_node->data=data;
+        new_node->left=NULL;
+        new_node->right=NULL;
+        
+        return new_node;
+    }
+    else
+    {
+        if(data>root->data)
+        {
+            root->right=createTree(root->right,data);
+        }
+        else
+        {
+            root->left=createTree(root->left,data);
+        }
+        return root;
+    }
+}
+
+void inorder(bst* root)
+{
+    if(root==NULL)
+        return;
+    else
+    {
+        inorder(root->left);
+        printf("%d ",root->data);
+        inorder(root->right);
+    }
+}
+
+int main()
+{
+    int n,i,a;
+    bst* root=NULL;
+    
+    printf("Enter nodes of binary search tree to enter\n");
+    
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a);
+        root = createTree(root,a);
+    }
+    
+    inorder(root);
+
+    return 0;
+}
